1c1bbb6714a46624cd91def0b55c97501d918e21
milleja46 <milleja46@gmail.com>
2012-05-01 15:32:42 -0400

    some changes to add some more functionality

--- simpleoffice/programs/simpleWord.py
+++ simpleoffice/programs/simpleWord.py
@@ -10,11 +10,13 @@
 import sys
 from PySide.QtCore import *
 from PySide.QtCore import *
-from .ui.simpleword_ui.Ui_Form imp
+from SimpleOffice.simpleoffice.ui.simpleword_ui.Ui_Form import *
 
 
 class widget(QWidget):
-
+    """
+    sets up the ui for the widget and makes connections to various buttons.
+    """
     def __init__(self):
         pass
 
@@ -23,3 +25,9 @@ class editorFunctions():
 
     def __init__(self):
         pass
+
+    def openFile(self, file):
+        pass
+
+if __name__ == "__main__":
+    print "Sorry I can't be run directly"
